75b5d0f6534dc0a49a96341f0233c35c864dd130
Nicholas Sim <nsim@posteo.net>
2022-04-19 21:00:17 +0800

    initial AsyncSeek impl

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,24 @@
 #![warn(missing_docs)]
 use std::cmp;
 use std::future::Future;
+use std::io::SeekFrom;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
 use futures::ready;
 use pin_project::pin_project;
 use postgres_types::Oid;
-use tokio::io::{AsyncRead, ReadBuf};
+use tokio::io::{AsyncRead, AsyncSeek, ReadBuf};
 use tokio_postgres::{Error, Row, Transaction};
 
 /// Default chunk size for reading/writing large object.
 ///
 /// 4M works well on my machine, not sure about yours.
 const DEFAULT_OP_SZ: usize = 4_194_304;
+
+const SEEK_SET: i32 = 0;
+const SEEK_CUR: i32 = 1;
+const SEEK_END: i32 = 2;
 
 async fn loread(client: &Transaction<'_>, fd: i32, size: usize) -> Result<Row, Error> {
     let size = cmp::min(size, DEFAULT_OP_SZ) as i32;
@@ -25,8 +30,25 @@ async fn loread(client: &Transaction<'_>, fd: i32, size: usize) -> Result<Row, E
         .await
 }
 
+
+async fn lseek64(client: &Transaction<'_>, fd: i32, whence: SeekFrom) -> Result<u64, Error> {
+    let (offset, whence) = match whence {
+        SeekFrom::Start(n) => (n as i64, SEEK_SET),
+        SeekFrom::Current(n) => (n, SEEK_CUR),
+        SeekFrom::End(n) => (n, SEEK_END),
+    };
+    client
+        .query_one(
+            "SELECT pg_catalog.lo_lseek64($1, $2, $3)",
+            &[&fd, &offset, &whence],
+        )
+        .await
+        .map(|row| row.get::<_, i64>(0) as u64)
+}
+
 #[pin_project(project = LoStateEn, project_replace=LoStateOwned)]
 enum LoState<'pin> {
+    Seek(Pin<Box<dyn Future<Output = Result<u64, Error>> + 'pin>>),
     Read(Pin<Box<dyn Future<Output = Result<Row, tokio_postgres::Error>> + 'pin>>),
     Wait,
 }
@@ -74,6 +96,34 @@ impl<'a> LargeObject<'a> {
     }
 }
 
+impl<'a> AsyncSeek for LargeObject<'a> {
+    fn start_seek(self: Pin<&mut Self>, position: SeekFrom) -> std::io::Result<()> {
+        let mut this = self.project();
+        let state = this.state.as_mut().project();
+        match state {
+            LoStateEn::Wait => {
+                let res = lseek64(*this.client, *this.fd, position);
+                this.state.project_replace(LoState::Seek(Box::pin(res)));
+                Ok(())
+            }
+            _ => todo!(),
+        }
+    }
+
+    fn poll_complete(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<u64>> {
+        let mut this = self.project();
+        let state = this.state.as_mut().project();
+        match state {
+            LoStateEn::Seek(fut) => {
+                let offset = ready!(fut.as_mut().poll(cx));
+                this.state.project_replace(LoState::Wait);
+                Poll::Ready(offset.map_err(|_| todo!()))
+            }
+            _ => todo!(),
+        }
+    }
+}
+
 impl<'a> AsyncRead for LargeObject<'a> {
     fn poll_read(
         self: Pin<&mut Self>,
@@ -109,6 +159,7 @@ impl<'a> AsyncRead for LargeObject<'a> {
                 cx.waker().wake_by_ref();
                 Poll::Pending
             }
+            _ => todo!(),
         }
     }
 }
